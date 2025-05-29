a0516a546c2b9b9865bda467ff1af7dd297fea88
gabrieldevreis <gabrielreisdev@gmail.com>
2025-05-28 23:22:06 -0300

    feat(frontend): adicionado rota de criar quiz

--- frontend/quiz-app/src/app/app.module.ts
+++ frontend/quiz-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { YourQuizzesComponent } from './your-quizzes/your-quizzes.component';
+import { CreateQuizComponent } from './create-quiz/create-quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    YourQuizzesComponent
+    YourQuizzesComponent,
+    CreateQuizComponent
   ],
   imports: [
     BrowserModule,
